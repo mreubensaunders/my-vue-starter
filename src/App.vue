9f0f82053d2c2ed1ae8379c06ffb568492d4f738
Michael Office <arcaneflorist@gmail.com>
2019-10-10 09:32:02 +0100

    scss handled in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app">    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,17 +16,48 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  /* #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+  } */
 </style>
 
 <style lang="sass">
+  @charset "utf-8"
+
+  // COLORS
+  $primary: #00d1b2
+  $info: #209CEE
+  $success: #23D160
+  $warning: #FFDD57
+  $danger: #FF3860
+  $light: #F5F5F5
+  $dark: #363636
+
+  // TEXT
+  $text: #4A4A4A
+  $link: #3273DC
+  $title-family: false
+  $button-family: false
+
+  // BREAKPOINTS
+  $gap: 32px
+  $tablet: 769px
+  $desktop: 960px + (2 * $gap)
+  $widescreen: 1152px + (2 * $gap)
+  $fullhd: 1344px + (2 * $gap)
+  $widescreen-enabled: true
+  $fullhd-enabled: false
+
+  // LAYOUT
+  $section-padding: 3rem 1.5rem
+  $section-padding-medium: 6rem 1.5rem
+  $section-padding-large: 9rem 1.5rem
+
   @import "~bulma/bulma.sass"
 </style>
 
